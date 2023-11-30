6cae3ba7339ae505f17f0b30c8af776d8afc82fa
Tamas Virag <virag.tamas2@gmail.com>
2023-11-30 17:01:51 +0100

    Hozz létre egy Szalloda osztályt, ami ezekből a Szobákból áll, és van saját attributuma is (név pl.)

--- hotel.py
+++ hotel.py
@@ -6,3 +6,24 @@ class Szoba(ABC):
     def __init__(self, szobaszam, ar):
         self.ar = ar
         self.szobaszam = szobaszam
+
+#Hozz létre az Szoba osztályból EgyagyasSzoba és KetagyasSzoba származtatott osztályokat, amelyek különböző attributumai vannak, és az áruk is különböző.(5 pont)
+
+class EgyagyasSzoba(Szoba):
+    def __int__(self):
+        self.ferohely = 1
+        self.ar = 20000
+
+
+class KetagyasSzoba(Szoba):
+    def __init__(self):
+        self.ferohely = 2
+        self.ar = 30000
+
+#Hozz létre egy Szalloda osztályt, ami ezekből a Szobákból áll, és van saját attributuma is (név pl.)
+
+class Szalloda:
+    def __int__(self):
+        self.szobak = []
+        self.nev = "Virág Tamás objektumorientált szállodája"
+
